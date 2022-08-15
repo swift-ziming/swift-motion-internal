efac3404da0e4acd17e89ff23c02a2c60f774c0c
ziming <liuzimingjay@swiftperformance.com>
2022-08-15 15:38:54 +1000

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Swift on 2022/8/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/SwiftMotionInternalApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMotionInternalApp.swift
+//  Shared
+//
+//  Created by Swift on 2022/8/15.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMotionInternalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
